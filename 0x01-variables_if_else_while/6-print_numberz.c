055c36c02ad1e81f0b1d3ead8f1a3568e503d3ee
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-05 12:41:28 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print function
+ *
+ * Description: A progarm that prints all single digit number of base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int dn;
+
+	for (dn = 0; dn < 10; dn++)
+	{
+		putchar(dn);
+	}
+		putchar('\n');
+
+	return (0);
+}
