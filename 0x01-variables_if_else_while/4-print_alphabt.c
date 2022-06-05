c46aad2209c1da8c97f348e8b5ff1d278a359672
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-05 11:40:24 -0700

    Task 4 done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - output function
+ *
+ * Descrption: A program that prints alphabet in lowercase except e and q
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	if (ch != 'e' && ch != 'q')
+		putchar(ch);
+	}
+		putchar('\n');
+
+	return (0);
+}
