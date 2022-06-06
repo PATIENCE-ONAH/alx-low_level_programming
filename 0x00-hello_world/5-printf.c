8c979508df6950f44ee1bd1b860c37d91d7575ce
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-06 08:57:23 -0700

    More task atempted

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Prints the exact statement followed by a new line.
+ *
+ * Return: 0
+ */
 
 int main(void)
 {
-printf(* with proper grammar, but the outcome is a piece of art, \n *);
-return (0);
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
