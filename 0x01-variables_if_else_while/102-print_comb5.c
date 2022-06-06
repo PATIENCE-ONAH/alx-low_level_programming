eba8bdd72b5b74c9023793bd789146c891837dbb
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-06 03:55:32 -0700

    C variables completed

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - prints function
+ *
+ * Description: prints all possible combinations of two two digit under 99
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 <= 98; n1++)
+	{
+		for (n2 = n1 + 1; n2 <= 99; n2++)
+		{
+			putchar((n1 / 10) + '0');
+			putchar((n1 % 10) + '0');
+			putchar(' ');
+			putchar((n2 / 10) + '0');
+			putchar((n2 % 10) + '0');
+
+			if (n1 == 98 && n2 == 99)
+
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+			putchar('\n');
+
+
+	return (0);
+}
