109bd751d025b72c7a9b4a1c167e5c7707ebb204
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-07 14:30:43 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet in lowercase
+ *
+ * Description: Prints 10 times the alphabet in lowercase and new line
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char ch;
+
+	while (count++ <= 9)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
