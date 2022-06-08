df107d359a604e882fe43aa5e3bae50644007123
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-08 15:20:04 -0700

    Advanced

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 & 2
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int count;
+
+	unisgned long f1 = 0, f2 = 1, sum;
+
+	for (count = 0; count < 50; count++)
+	{
+		sum = f1 + f2;
+		printf("%lu", sum);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
