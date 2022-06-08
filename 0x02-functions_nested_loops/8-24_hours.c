ea1b08083e586b842463797671e24d4a741e6351
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-08 02:34:21 -0700

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints every minutes of the day of Jack Bauer
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hour;
+	int munite;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '5');
+			_putchar((minute % 10) + '9');
+			_putchar('\n');
+		}
+	}
+}
