5356b6063c7cefe0db74300995b98f4293fc288c
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-08 04:26:44 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Adds two intergers and return their result
+ *
+ * @n1: First interger
+ * @n2: Second interger
+ *
+ * Return: The result of addition
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
