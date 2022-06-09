6260e4208825d457cf1480ea6590d72b0a679629
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-09 14:03:41 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: sencond integer
+ * Return: result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	return ( a * z);
+}
