fd7e1a4c2b2992c48db3441a219c5e4aeabc685d
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-10 07:54:03 -0700

    Correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,13 +13,13 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3) == 0 && (n % 5) == 0)
-			printf("fizzbuzz");
+			printf("FizzBuzz");
 
 		else if ((n % 3) == 0)
-			printf("fizz");
+			printf("Fizz");
 
 		else if ((n % 5) == 0)
-			printf("buzz");
+			printf("Buzz");
 
 		else
 			printf("%d", n);
