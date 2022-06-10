5d4b352d91e0e7d6eac4f64a4db08742c3c122c3
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-10 04:59:17 -0700

    Debugging completed

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - Draws a diagonal line using the character \\
+ * @n: The number of diagonal \ character to be printed
+ */
+
+void print_diagonal(int n)
+{
+	int diago, spcae;
+
+	if (n > 0)
+	{
+		for (diago = 0; diago < n; diago++)
+		{
+			for (space = 0; space < diago; space++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (diago == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
