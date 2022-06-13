2f99a3806a60f37cdbdd99da6f8fb6c139988e7b
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-13 15:46:35 -0700

    correction

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,8 +12,7 @@ char *_strcpy(char *dest, const char *src)
 {
 	int index = 0;
 
-	while (src[index] != '\0')
-		dest[index] = '\n';
+	while (src[index])
 	{
 		dest[index] = src[index];
 		index++;
