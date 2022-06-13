be5b333fadf3655d9f6f89974ee42d61bc1015e0
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-13 12:37:23 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - prints string in reverse
+ * @s: The string
+ */
+
+void print_rev(char *s)
+{
+	int length, index;
+
+	while (s[index++])
+		length++;
+
+	for (index = length - 1; index >= 0; index--)
+		_putchar(s[index]);
+
+	_putchar('\n');
+}
