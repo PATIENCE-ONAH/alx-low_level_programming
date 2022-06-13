9cb86956108e5480f5f1a81cf6c3d78c0c535f1d
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-13 15:23:57 -0700

    correction

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src, including 
+ * _strcpy - copies the string pointed to by src, including
  * the terminating null byte (\0), to the buffer pointed to by dest
  * @dest: A buffer to copy the string to
  * @src: The source string to copy.
