ca3ce6471b6362a0b138f5c7d78a1a185dbc43c5
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-13 05:48:41 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#inclide "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ * Return: length of s
+ */
+
+int _strlen(char *s)
+{
+	int length = 0
+
+	while (*s++)
+		length++;
+	return (length);
+}
