1c01b6bba471ad09a1ad67ee278ff5ca0ad1ca0f
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-06-13 04:27:37 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two intergers
+ * @a: First integer
+ * @b: Second interger
+ */
+
+void swap_int(int *a, *b)
+{
+	int i = *a;
+	*a = *b;
+	*b = i;
+}
