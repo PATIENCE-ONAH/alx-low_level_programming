3099f5cac2857fa214084f5f0563098c05f231b3
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-07-04 14:23:06 -0700

    edited task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,4 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
-/**
- * file: 3-function_like_macro.h
- * @x: the value of the number to be computed
- * Decription: A function-like macro ABS(x) that computes
- * the absolute value of the number x
- */
-#define ABS(X) (x >= 0 ? (x) : (-x))
-
+#define ABS(X) ((x) < 0 ? (-x) : (x))
 #endif
