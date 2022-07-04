e58519d8b03cf34532c4dbedfba9c9e3d1f19060
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2022-07-04 05:49:28 -0700

    preprocessor

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro.h.
+ * Decription: A header file that defines a macros named SIZE
+ * as an abrievation for the token 1024
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,5 @@
+C - PREPROCESSOR
+In this project I learnt the following:
+1.	what are macros and how to use them
+2.	what are the common predefined macros
+3.	how to include guard your header files.
